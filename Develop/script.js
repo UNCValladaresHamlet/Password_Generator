6a19e452af8a4c2980d2b420e48d6565896ab740
Hamlet Valladares <sigorahv@gmail.com>
2021-04-16 23:45:01 -0400

    Added if statement to the generate password function

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,7 @@
 // Pseudo code for password hw
 // Product managers
-// 1. Ask how many characters the password should be
-// 2. Validate that the password length is at least 8 characters. If not, throw an alert
+// 1. Ask how many characters the password should be ✓
+// 2. Validate that the password length is at least 8 characters. If not, throw an alert ✓
 // 3. Ask if they want special, numerical, uppercase, lowercase characters
 // 4. I need a way to store all these characters, the numerical, special, upper, lower. Probably a variable
 // 5. Start building the password
@@ -14,27 +14,35 @@
 //  W8l!a@2
 // 7. Display the password
 
-//Ask how many characters the password should be
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-var uppercaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J','K', 'L', 'M', 'N', 'O', 'P', 'G', 'H', 'I', 'J']
-var lowercaseCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', '8', '9']
-var symbolsCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
+// console.log('character:',numericCharacters)
+// Arrays 
+var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numberArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var symbolArray = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 
-console.log('character:',numericCharacters)
+var length = "";
+var confirmspecialCharacters;
+var confirmnumericalCharacters;
+var confirmuppercaseCharacters;
+var confirmlowercaseCharacters;
+var newgeneratedpassword = "";
+const validatedCharacterArray = [];
 
-function getPasswordOptions() {
+function getpasswordCriteria() {
   // Variable to store length of password from user input
-  var length = parseInt(
-    prompt('How many characters would you like your password to contain?')
+  length = parseInt(
+    prompt('How many characters would you like your password to contain?') // 1.Ask how many characters the password should be
   );
-  // Conditional statement to check if password length is a number. Prompts end if this evaluates false
+  
+    // Conditional statement to check if password length is a number. Prompts end if this evaluates false
   if (isNaN(length) === true) {
-    alert('Password length must be provided as a number');
+    alert('Password length must be provided as a number'); 
     return;
   }
   // Conditional statement to check if password length is at least 8 characters long. Prompts end if this evaluates false
   if (length < 8) {
-    alert('Password length must be at least 8 characters');
+    alert('Password length must be at least 8 characters'); // 2. Validate that the password length is at least 8 characters. If not, throw an alert (line above)
     return;
   }
   // Conditional statement to check if password length is less than 128 characters long. Prompts end if this evaluates false
@@ -43,14 +51,53 @@ function getPasswordOptions() {
     return;
   }
   // Variable to store boolean regarding the inclusion of special characters
-  var hasSpecialCharacters = confirm(
-    'Click OK to confirm including special characters.'
+  // 3. Ask if they want special, numerical, uppercase, lowercase characters
+  confirmSpecialCharacters = confirm(
+    'Click OK to confirm you would like to include special characters'  // 4. I need a way to store all these characters, the numerical, special, upper, lower. Probably a variable
   );
+  console.log('confirmspecialCharacters:',confirmspecialCharacters)
+  confirmNumericalCharacters = confirm(
+    'Click OK to confirm you would like to include numerical characters' // 4. I need a way to store all these characters, the numerical, special, upper, lower. Probably a variable
+  );
+  console.log('confirmnumericalCharacters:',confirmnumericalCharacters)
+  confirmLowercaseCharacters = confirm(
+    'Click OK to confirm you would  like to include lowercase characters' // 4. I need a way to store all these characters, the numerical, special, upper, lower. Probably a variable
+  );
+  console.log('confirmlowercaseCharacters:',confirmlowercaseCharacters)
+  confirmUppercaseCharacters = confirm(
+    'Click OK to confirm you would like to include uppercase characters' // 4. I need a way to store all these characters, the numerical, special, upper, lower. Probably a variable
+  );
+  console.log('confirmuppercaseCharacters:',confirmuppercaseCharacters)
+  
+  generatePassword()
+
+  }
+
+  function generatePassword() {
+    if (confirmLowercaseCharacter) {
+      for (var index = 0; index < lowercaseArray.length; index++) {
+          validatedCharacterArray.append(lowercaseArray[index]);
+      }
+    }
+    if (confirmUppercaseCharacters) {
+      for (var index = 0; index < uppercaseArray.length; index++) {
+          validatedCharacterArray.append(uppercaseArray[index]);
+      }
+    }
+    if (confirmSpecialCharacters) {
+      for (var index = 0; index < symbolArray.length; index++) {
+          validatedCharacterArray.append(symbolArray[index]);
+      }
+    }
+    if (confirmNumericalCharacters) {
+      for (var index = 0; index < numberArray.length; index++) {
+          validatedCharacterArray.append(numberArray[index]);
+      }
+    }
+
   }
   
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
@@ -61,11 +108,8 @@ function writePassword() {
 
 }
 
-function generatePassword() {
-  
-
-}
-
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", getPasswordOptions());
+generateBtn.addEventListener("click", getpasswordCriteria());
